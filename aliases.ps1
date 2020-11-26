c79ccb97de4173c940a98424c4327f795f995592
Devyn Spencer <devynspencer@users.noreply.github.com>
2020-11-25 19:22:41 -0800

    Create aliases.ps1

---
+++ aliases.ps1
@@ -0,0 +1,14 @@
+# Standard functions
+New-Alias gs Get-PSSession
+New-Alias rs Remove-PSSession
+
+New-Alias gj Get-Job
+New-Alias rj Remove-Job
+New-Alias cj Receive-Job
+
+New-Alias c Clear-Host
+New-Alias which Get-Command
+
+New-Alias gadc Get-ADComputer
+New-Alias gadd Get-ADDomain
+New-Alias gaddc Get-ADDomainController
