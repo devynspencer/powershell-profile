f14a4442275251d805790fe34702b276ffe64ed8
Devyn Spencer <devynspencer@users.noreply.github.com>
2020-11-26 18:20:41 -0800

    Update Join-Profile.ps1

--- Scripts/Join-Profile.ps1
+++ Scripts/Join-Profile.ps1
@@ -3,4 +3,5 @@ $DateSlug = (Get-Date).ToString('yyyyMd')
 $ExportFileName = "$DateSlug-$(New-Guid).Profile.ps1"
 
 # Combine individual functions from files and export into new file
+# TODO: Add parameters "Force" & "ProfilePath": overwrite existing profile at $ProfilePath
 $Files | Get-Content | Out-File -FilePath $ExportFileName
